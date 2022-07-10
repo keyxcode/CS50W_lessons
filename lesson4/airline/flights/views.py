8d97d6d813d5f1d43bec6b249c618a9e141c2edd
keyxcode <keyxcode@gmail.com>
2022-07-10 15:26:11 +0700

    update

--- lesson4/airline/flights/views.py
+++ lesson4/airline/flights/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import *
 
@@ -11,10 +12,11 @@ def index(request):
 
 
 def flight(request, flight_id):
-    flight = Flight.objects.get(id=flight_id)
+    flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 
@@ -23,4 +25,4 @@ def book(request, flight_id):
         flight = Flight.objects.get(pk=flight_id)
         passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
         passenger.flights.add(flight)
-        return HttpResponseRedirect(reverse("flight", args=(flight.id, )))
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
