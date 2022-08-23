469c09c45b90faf1d7a1d5d933ad4fe343465de0
keyxcode <keyxcode@gmail.com>
2022-08-23 14:39:47 +0700

    add automated sel tests

---
+++ lesson7/selenium/tests.py
@@ -0,0 +1,48 @@
+import os
+import pathlib
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Finds the Uniform Resource Identifier (URI) of a file
+def file_uri(filename):
+    return pathlib.Path(os.path.abspath(filename)).as_uri()
+
+
+# Sets up web driver using Chrome
+driver = webdriver.Chrome("/Users/minh-quan/repo/CS50W/CS50W_lessons/lesson7/selenium/chromedriver")
+
+
+class WebpageTests(unittest.TestCase):
+
+    def test_title(self):
+        """Make sure title is correct"""
+        driver.get(file_uri("counter.html"))
+        self.assertEqual(driver.title, "Counter")
+
+    def test_increase(self):
+        """Make sure header updated to 1 after 1 click of increase button"""
+        driver.get(file_uri("counter.html"))
+        increase = driver.find_element(By.ID, "increase")
+        increase.click()
+        self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "1")
+
+    def test_decrease(self):
+        """Make sure header updated to -1 after 1 click of decrease button"""
+        driver.get(file_uri("counter.html"))
+        decrease = driver.find_element(By.ID, "decrease")
+        decrease.click()
+        self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "-1")
+
+    def test_multiple_increase(self):
+        """Make sure header updated to 3 after 3 clicks of increase button"""
+        driver.get(file_uri("counter.html"))
+        increase = driver.find_element(By.ID, "increase")
+        for i in range(3):
+            increase.click()
+        self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "3")
+
+
+if __name__ == "__main__":
+    unittest.main()
